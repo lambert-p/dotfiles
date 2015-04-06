3301c3302423c3271c9f709fd95dca439ad66e8d
Paul Lambert <lambert.m.paul@me.com>
2015-04-06 17:31:30 -0500

    updating package sources, finally making the switch to evil mode... maybe

--- emacs/init.el
+++ emacs/init.el
@@ -6,24 +6,28 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'zenburn t)
 
-;; Set up lisp system for SLIME
-(setq inferior-lisp-program "/usr/local/bin/sbcl")
-(setq slime-contribs '(slime-fancy))
+;; rebind <RET> to newline-and-indent (default C-j)
+(add-hook 'c-mode-hook
+	  '(lambda ()
+	     (define-key c-mode-map "\C-m" 'newline-and-indent)))
 
+;; add package manager sources
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                         ("gnu" . "htp://elpa.gnu.org/packages/")))
+
+;; initialize package manger
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/") t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("53e29ea3d0251198924328fd943d6ead860e9f47af8d22f0b764d11168455a8e" "e4e97731f52a5237f37ceb2423cb327778c7d3af7dc831788473d4a76bcc9760" "fe20c1ea61a2836a5cea69963865b5b8df8c480ccaf3f11ad7f2e1f543f6c274" "bb6b64bfb2f63efed8dea1ca03691c07c851a8be6f21675fe4909289d68975d9" "4a60f0178f5cfd5eafe73e0fc2699a03da90ddb79ac6dbc73042a591ae216f03" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(package-initialize)
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; KEEP AT BOTTOM
+;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; enable evil-mode
+
+;; make C-u scroll up
+(setq evil-want-C-u-scroll t)
+
+(require 'evil)
+(evil-mode 1)
