05aba2500b3f49aab6247c7b9966acb585a42096
Paul Lambert <lambert.m.paul@me.com>
2015-04-06 18:26:34 -0500

    updating config for evil-mode and setting started with emacs. will extract to other files once i know what i'm doing...

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,24 @@
+;; init.el for Paul Lambert < lambertington @ gmail.com >
+;; github.com/lambertington
+
+;; initialize package manger
+(require 'package)
+(package-initialize)
+(setq package-enable-at-startup nil)
+
+;; set up custom load path
+(add-to-list 'load-path (concat user-emacs-directory "config"))
+
+;; add package manager sources
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                         ("gnu" . "htp://elpa.gnu.org/packages/")))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
 ;; hide the menu bar and tool bar
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -11,23 +32,39 @@
 	  '(lambda ()
 	     (define-key c-mode-map "\C-m" 'newline-and-indent)))
 
-;; add package manager sources
-(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
-                         ("org" . "http://orgmode.org/elpa/")
-                         ("gnu" . "htp://elpa.gnu.org/packages/")))
+;; use superior Lisp interaction mode
+(progn
+  (require 'elisp-slime-nav)
+  (defun my-lisp-hook ()
+    (elisp-slime-nav-mode)
+    (turn-on-eldoc-mode))
+  (add-hook 'emacs-lisp-mode-hook 'my-lisp-hook))
 
-;; initialize package manger
-(require 'package)
-(package-initialize)
+;; hide the startup messages
+(setq inhibit-splash-screen t
+      inhibit-startup-echo-area-message t
+      inhibit-startup-message t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;
-;; KEEP AT BOTTOM
-;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; evil mode configuration ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; enable evil-mode
+(require 'evil)
+(evil-mode 1)
 
 ;; make C-u scroll up
 (setq evil-want-C-u-scroll t)
 
-(require 'evil)
-(evil-mode 1)
+;; map M-x describe-function to 'k'
+(evil-define-key 'normal emacs-lisp-mode-map (kbd "K")
+  'elisp-slime-nav-describe-elisp-thing-at-point)
+
+;; esc ALWAYS quits
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
