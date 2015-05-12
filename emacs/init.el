784c0ea458958d7b6289e36e1c1cebca7233aa6d
Paul Lambert <lambert.m.paul@me.com>
2015-05-12 12:30:28 -0500

    adding web-mode for handling erbs, etc

--- emacs/init.el
+++ emacs/init.el
@@ -22,6 +22,17 @@
 (unless (package-installed-p 'cider)
   (package-install 'cider))
 
+;; activate web-mode
+(require 'web-mode)
+(defun my-web-mode-hook()
+  "Hooks for web-mode"
+  (setq web-mode-markup-indent-offset 2))
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+
 ;; activate emacs code browser
 (require 'ecb)
 
