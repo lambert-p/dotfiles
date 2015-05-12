09b1421e48485908a4b3130350fe588c4551efc1
Paul Lambert <lambert.m.paul@me.com>
2015-05-12 12:43:14 -0500

    adding support for line numbers on left

--- emacs/config/my-setup.el
+++ emacs/config/my-setup.el
@@ -4,6 +4,17 @@
 ;; hide the menu bar and tool bar
 (menu-bar-mode -1)
 (tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq current-language-environment "English")
+
+(line-number-mode t)
+(column-number-mode t)
+(global-linum-mode t)
+
+(setq-default indent-tabs-mode nil)
+
+;; change yes-or-no to y-or-n
+(fset 'yes-or-no-p 'y-or-n-p)
 
 ;; color scheme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
