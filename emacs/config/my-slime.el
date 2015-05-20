15dacacd823d9b7346ab78dc671d12c0cf28a2e8
Paul Lambert <lambert.m.paul@me.com>
2015-05-19 22:21:39 -0500

    adding config for SLIME

--- emacs/config/my-slime.el
+++ emacs/config/my-slime.el
@@ -2,7 +2,7 @@
 ;; sets up behavior for SLIME, superior lisp interaction mode for emacs
 
 
-;; hook up SLIME to SBCL
+; hook up SLIME to SBCL
 (setq inferior-lisp-program "/usr/local/bin/sbcl")
 
 (provide 'my-slime)
