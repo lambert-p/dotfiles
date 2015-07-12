b5f291f9c1065fc9254d9194a49100546e05ffd3
Paul Lambert <lambert.m.paul@me.com>
2015-07-12 17:36:51 -0500

    moved package management out of init

---
+++ emacs.d/config/my-packages.el
@@ -0,0 +1,34 @@
+;;;; my-packages.el
+;;;; handle package initialization 
+
+(require 'cl)
+
+;;; initialize package manger
+(require 'package)
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+(setq package-enable-at-startup nil)
+
+;;; list taken from `C-h v package-activated-list`
+(defvar my-packages
+  '(bind-key cider-decompile javap-mode cider queue pkg-info epl dash
+             clojure-mode cider-eval-sexp-fu eval-sexp-fu highlight
+             cider-profile cider-spy diminish elisp-slime-nav
+             evil goto-chg undo-tree ghc haskell-mode
+             haskell-emacs magit magit-popup git-commit with-editor
+             markdown-mode paredit python-mode slime typescript-mode
+             web-mode yaml-mode)
+  "A list of packages to ensure are installed at launch.")
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(dolist (package my-packages)
+  (when (and (not (package-installed-p package))
+             (assoc package package-archive-contents))
+    (package-install package)))
+
+(provide 'my-packages)
+
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,41 +1,16 @@
 ;;;; init.el for Paul Lambert < lambertington @ gmail.com >
 ;;;; github.com/lambertington
 
-(require 'cl)
-
-;;; initialize package manger
-(require 'package)
-(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
-                         ("org" . "http://orgmode.org/elpa/")
-                         ("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize)
-(setq package-enable-at-startup nil)
-
-;;; list taken from `C-h v package-activated-list`
-(defvar my-packages
-  '(bind-key cider-decompile javap-mode cider queue pkg-info epl dash
-             clojure-mode cider-eval-sexp-fu eval-sexp-fu highlight
-             cider-profile cider-spy diminish elisp-slime-nav
-             evil goto-chg undo-tree ghc haskell-mode
-             haskell-emacs magit magit-popup git-commit with-editor
-             markdown-mode paredit python-mode slime typescript-mode
-             web-mode yaml-mode)
-  "A list of packages to ensure are installed at launch.")
-
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-(dolist (package my-packages)
-  (when (and (not (package-installed-p package))
-             (assoc package package-archive-contents))
-    (package-install package)))
-
-;;; set up custom load path
+;; set up custom load path
 (add-to-list 'load-path (concat user-emacs-directory "config"))
 
-;;; include all my stuff, located in ./config
+;; set up packages
+(require 'my-packages)
+
+;; set up some reasonable defaults
 (require 'my-setup)
 
+;; include all my stuff, located in ./config
 (require 'my-evil)
 (require 'my-helpers)
 (require 'my-javascript-mode)
