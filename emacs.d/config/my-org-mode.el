a2913b0cf02f652ac3d417f5a6ece0b3ee1222da
Paul Lambert <lambert.m.paul@me.com>
2015-07-18 19:42:23 -0500

    adding org-mode config...

---
+++ emacs.d/config/my-org-mode.el
@@ -0,0 +1,5 @@
+;;;; my-org-mode.el
+;;;; includes helpful defaults for org-mode functionality
+
+
+(provide 'my-org-mode)
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -16,6 +16,7 @@
 (require 'my-helpers)
 (require 'my-javascript-mode)
 ; (require 'my-magit)
+(require 'my-org-mode)
 (require 'my-ruby-mode)
 (require 'my-slime)
 (require 'my-web-mode)
