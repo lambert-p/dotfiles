2a00fa81448c0f33e37166df9edafa78dad3af86
Paul Lambert <lambert.m.paul@me.com>
2018-01-18 19:58:59 +0000

    fixing font size on Arch

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,7 +1,28 @@
 ;;;; init.el for Paul Lambert < lambertington @ gmail.com >
 ;;;; github.com/lambertington
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'org)
 
 (setq custom-org "~/.emacs.d/lambert-config.org")
 (org-babel-load-file (expand-file-name custom-org))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (magit evil web-mode python-mode lua-mode slime js2-mode ghc coffee-mode cider auto-compile use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
