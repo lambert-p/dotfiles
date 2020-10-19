6e17ff7295763f03f058bd0ff461a57d5cb75fa5
Paul Lambert <paul.lambert@linux.com>
2020-10-19 10:32:09 -0500

    adding Markdown-mode. quite convenient.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -19,7 +19,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (magit evil web-mode python-mode lua-mode slime js2-mode ghc coffee-mode cider auto-compile use-package))))
+    (markdown-mode magit evil web-mode python-mode lua-mode slime js2-mode ghc coffee-mode cider auto-compile use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
